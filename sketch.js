044e8481fabbf799467ea4ffd7247cccdae8ff9a
JenniSilva <jeje.sprimo16@gmail.com>
2024-08-27 20:58:29 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function setup() {
+    createCanvas(400, 400);
+    background("black")
+  }
+  
+  function draw() {
+    stroke("pink");
+    fill("pink");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
